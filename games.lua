4f239d8e01753323d43b2e4ff8524061fb23fbdd
6lvls <zevywevyy@gmail.com>
2026-03-06 16:41:16 -0500

    Update games.lua
    check

--- games.lua
+++ games.lua
@@ -1,18 +1,1 @@
-return {
-
-[2788229376] = {
-    name = "da hood",
-
-    scripts = {
-
-        {
-            name = "Auto Reset",
-            description = "Automatically resets your character every 2 minutes",
-            icon = "rbxassetid://123456",
-            url = "https://raw.githubusercontent.com/6lvls/hub/refs/heads/main/scripts/auto.lua",
-            tab = "Player"
-
-    }
-}
-
-}
+print('hi')
